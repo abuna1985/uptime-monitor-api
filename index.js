73ceca179afdc01f6132c78fe0daba589cdd0498
Adam Abundis <adamjabundis@gmail.com>
2018-09-11 20:35:42 -0700

    index - collect headers and payload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all request with a string
 const server = http.createServer((req, res) => {
@@ -22,11 +23,22 @@ const server = http.createServer((req, res) => {
     // Get the HTTP Method
     const method = req.method.toLowerCase();
 
-    // Send the response
-    res.end('Hello, World\n');
+    // Get the headers as an object
+    const headers = req.headers;
 
-    // Log the request path
-    console.log(`Request received on path: ${trimmedPath} with this method: ${method} and with these query string parameters: %j`, queryStringObject);
+    // Get the payload, if any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', data => buffer += decoder.write(data));
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // Send the response
+        res.end('Hello, World\n');
+
+        // Log the request path
+        console.log('Request received with this payload: ', buffer);
+    });
 });
 // Start the server, and have it listen on port 3000
 server.listen(3000, () => console.log('The server is listening on port 3000 now'));
